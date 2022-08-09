05be449ea6046c754a3542a8784d3853d96f0318
Wagner <wagner_silvajr@hotmail.com>
2022-08-09 09:50:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudo_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
